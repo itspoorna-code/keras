e182ffb44a3fc2b0249999ab58977ebf8a4717c1
itspoorna-code <itspoorna011@gmail.com>
2024-11-04 16:22:18 +0530

    init

---
+++ linear.py
@@ -0,0 +1,27 @@
+import keras 
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD,Adam
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.random.rand(100,1)
+
+y=2*x+0.005
+# print(y)
+
+model=Sequential()
+model.add(Dense(2,input_dim=1))
+
+model.add(Dense(1))
+model.compile(optimizer=Adam(learning_rate=0.001),loss='mean_squared_error')
+
+model.fit(x,y,epochs=500)
+output=model.predict(x)
+# print(output)
+
+plt.scatter(x,y,label='original data')
+plt.plot(x,output,label='predicted data')
+
+plt.show()
+# plt.plot()
